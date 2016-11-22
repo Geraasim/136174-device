ef846219c169fd1c58eac0cd2ed36dc50981579f
Geraasim <tneidergni@gmail.com>
2016-11-23 01:31:11 +0300

    куролес 23/11

--- js/js.js
+++ js/js.js
@@ -27,3 +27,36 @@ writeModal.classList.remove("modal-show");
 });
 
 });
+
+
+document.querySelector(".btn-write").addEventListener("click", function (event) {
+event.preventDefault();
+
+var writeModal = document.querySelector(".modal-content");
+var writeUsClose = writeModal.querySelector(".modal-content-close");
+writeModal.classList.add("modal-show");
+
+var Overlay = document.querySelector(".modal-overlay");
+var OverlayClose = writeModal.querySelector(".modal-content-close");
+Overlay.classList.add("modal-show");
+
+OverlayClose.addEventListener("click", function (event) {
+event.preventDefault();
+OverlayModal.classList.remove("modal-show");
+});
+
+});
+
+document.querySelector(".map").addEventListener("click", function (event) {
+event.preventDefault();
+
+var mapPopup = document.querySelector(".modal-content-map");
+var mapClose = mapPopup.querySelector(".modal-content-close");
+mapPopup.classList.add("modal-content-show");
+
+mapClose.addEventListener("click", function (event) {
+event.preventDefault();
+mapPopup.classList.remove("modal-content-show");
+});
+
+});
