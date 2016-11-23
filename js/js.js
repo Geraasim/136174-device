79c5f4f2e87600f62c83d3a16815c89823266add
Geraasim <tneidergni@gmail.com>
2016-11-23 21:07:39 +0300

    куролес 23/11 ИТОГ

--- js/js.js
+++ js/js.js
@@ -32,31 +32,39 @@ writeModal.classList.remove("modal-show");
 document.querySelector(".btn-write").addEventListener("click", function (event) {
 event.preventDefault();
 
-var writeModal = document.querySelector(".modal-content");
-var writeUsClose = writeModal.querySelector(".modal-content-close");
-writeModal.classList.add("modal-show");
+// var writeModal = document.querySelector(".modal-content");
+// var writeUsClose = writeModal.querySelector(".modal-content-close");
+// writeModal.classList.add("modal-show");
 
 var Overlay = document.querySelector(".modal-overlay");
-var OverlayClose = writeModal.querySelector(".modal-content-close");
+var OverlayClose = Overlay.querySelector(".modal-content-close");
 Overlay.classList.add("modal-show");
 
 OverlayClose.addEventListener("click", function (event) {
 event.preventDefault();
-OverlayModal.classList.remove("modal-show");
+Overlay.classList.remove("modal-show");
 });
 
 });
 
-document.querySelector(".map").addEventListener("click", function (event) {
+
+document.querySelector(".btn-write").addEventListener("click", function (event) {
 event.preventDefault();
 
-var mapPopup = document.querySelector(".modal-content-map");
-var mapClose = mapPopup.querySelector(".modal-content-close");
-mapPopup.classList.add("modal-content-show");
+var writeModal = document.querySelector(".modal-content");
+var writeUsClose = writeModal.querySelector(".modal-content-close");
+var Overlay = document.querySelector(".modal-overlay");
+var OverlayClose = Overlay.querySelector(".modal-content-close");
+Overlay.classList.add("modal-show");
+writeModal.classList.add("modal-show");
 
-mapClose.addEventListener("click", function (event) {
+writeUsClose.addEventListener("click", function (event) {
 event.preventDefault();
-mapPopup.classList.remove("modal-content-show");
+writeModal.classList.remove("modal-show");
+
+var Overlay = document.querySelector(".modal-overlay");
+var OverlayClose = Overlay.querySelector(".modal-content-close");
+Overlay.classList.add("modal-show");
 });
 
 });
